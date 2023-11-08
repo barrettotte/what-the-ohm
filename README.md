a460af78f2ac4f229f49cea2539c630f9ad74eb6
Barrett Otte <barrettotte@gmail.com>
2023-11-07 20:23:49 -0500

    adds dataset script

--- README.md
+++ README.md
@@ -8,7 +8,15 @@ TODO:
 
 ## Dataset
 
-https://www.kaggle.com/datasets/eralpozcan/resistor-dataset
+Images of 4 and 5 band resistors taken with USB microscope.
+
+- 67 values of 4 band resistors
+- 60 values of 5 band resistors
+- 1270 images in total
+
+Dataset prepped and bundled using `dataset.py`.
+
+Finalized dataset hosted at https://www.kaggle.com/datasets/barrettotte/resistors
 
 ## Development
 
@@ -20,6 +28,5 @@ conda activate what-the-ohm
 
 ## References
 
-- https://www.kaggle.com/datasets/eralpozcan/resistor-dataset
 - https://pytorch.org/docs/stable/index.html
 - https://www.digikey.com/en/resources/conversion-calculators/conversion-calculator-resistor-color-code
---
+++ dataset.py
@@ -0,0 +1,71 @@
+# build CSV of resistor images and their respective values
+# also prepare zip file for kaggle dataset
+
+import csv
+import json
+import os
+import shutil
+import traceback
+from pathlib import Path
+
+WATTS = 0.25
+DATA_ROOT = Path('./data')
+
+# parse resistor values from filenames
+def parse_filename(f: str) -> dict:
+    try:
+        split = f.split('-')
+        ohms = split[1]
+
+        if 'R' in ohms:
+            i = ohms.find('R')
+            hundreds = int(ohms[:i])
+            decimal = 0 if i == len(ohms) - 1 else (int(ohms[i+1]) / 10.0)
+            ohms = hundreds + decimal
+        elif 'K' in ohms:
+            i = ohms.find('K')
+            thousands = (int(ohms[:i]) * 1000)
+            hundreds = 0 if i == len(ohms) - 1 else (int(ohms[i+1]) * 100)
+            ohms = thousands + hundreds
+        elif 'M' in ohms:
+            i = ohms.find('M')
+            millions = (int(ohms[:i]) * 1000000)
+            thousands = 0 if i == len(ohms) - 1 else (int(ohms[i+1]) * 100000)
+            ohms = millions + thousands
+        else:
+            raise ValueError('Bad filename found', f)
+        
+        band_count = int(split[0].replace('B', ''))
+        tolerance = float(split[2].replace('T', '')) / 100.0
+
+        with open(DATA_ROOT / f'{band_count}-band{os.sep}{band_count}-band.json', 'r') as bands_f:
+            band_mapping = json.load(bands_f)
+        bands = ' '.join(band_mapping[str(ohms)])
+        ohms = float(ohms)
+
+        return {'band_count': band_count, 'ohms': ohms, 'tolerance': tolerance, 'watts': WATTS, 'bands': bands}
+    
+    except Exception:
+        print('Failed to parse', f)
+        print(traceback.format_exc())
+        exit(1)
+
+def main():
+    with open(DATA_ROOT / 'train.csv', 'w+', newline='', encoding='utf-8') as f:
+        csv_writer = csv.writer(f)
+
+        for i, p in enumerate(Path(DATA_ROOT).rglob('*.jpg')):
+            r = parse_filename(p.name)
+            if i == 0:
+                header = ['image']
+                header.extend(list(r.keys()))
+                csv_writer.writerow(header)
+            
+            img_path = os.path.join(*os.path.split(p)[0].split(os.sep)[1:], p.name)
+            row = [img_path, r['band_count'], r['ohms'], r['tolerance'], r['watts'], r['bands']]
+            csv_writer.writerow(row)
+    
+    shutil.make_archive('resistors', 'zip', DATA_ROOT)
+
+if __name__ == '__main__':
+    main()
